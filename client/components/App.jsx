78912118e21d21260019e27ce5f64c6d038817ce
Emma Li <emma.m.li.98@gmail.com>
2020-11-20 17:34:51 -0600

    Adds column formatting to sidebar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
 import Sidebar from './Sidebar.jsx';
 import About from './About.jsx';
 import Coding from './Coding.jsx';
 import Animation from './Animation.jsx';
 import Contact from './Contact.jsx';
 
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+`;
+
 const App = () => {
   const [current, setCurrent] = useState('about');
 
   return (
-    <div>
+    <Wrapper>
       <Sidebar focused={current} changeFocus={setCurrent} />
       {current === 'about' && <About />}
       {current === 'coding' && <Coding />}
       {current === 'animation' && <Animation />}
       {current === 'contact' && <Contact />}
-    </div>
+    </Wrapper>
   )
 }
 
