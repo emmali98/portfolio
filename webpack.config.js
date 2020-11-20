d132a973396fa07c3b30f9cb8ddb1e9d0acdd2a6
Emma Li <emma.m.li.98@gmail.com>
2020-11-20 15:19:57 -0600

    Resets to using React for single-page app

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve('.', 'client', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'portfolio.bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
