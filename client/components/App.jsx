b314ad9a3220104adc4ef4b84b79772384dc3943
Emma Li <emma.m.li.98@gmail.com>
2020-11-24 12:52:40 -0600

    Adds background colors and transition

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,14 +9,24 @@ import Contact from './Contact.jsx';
 
 const Wrapper = styled.div`
   display: grid;
+  height: 100vh;
   grid-template-columns: 1fr 4fr;
+  background: ${props => props.bgColor};
+  transition: background 2s ease;
 `;
+
+const bgColors = {
+  about: 'rgb(255, 239, 180)',
+  coding: 'rgb(255, 198, 138)',
+  animation: 'rgb(226, 129, 103)',
+  contact: 'rgb(40, 37, 87)'
+};
 
 const App = () => {
   const [current, setCurrent] = useState('about');
 
   return (
-    <Wrapper>
+    <Wrapper bgColor={bgColors[current]}>
       <Sidebar focused={current} changeFocus={setCurrent} />
       {current === 'about' && <About />}
       {current === 'coding' && <Coding />}
