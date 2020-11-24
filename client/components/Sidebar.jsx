6f51f4644bcffa31bf01b86b3fcd8321dbba7e62
Emma Li <emma.m.li.98@gmail.com>
2020-11-24 15:45:20 -0600

    Adds sidebar text color styling

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -11,9 +11,19 @@ const Link = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  color: ${props => props.focused ? 'black' : 'palevioletred'};
+  font-size: 2em;
+  font-weight: ${props => props.selected ? '600' : '200'};
+  color: ${props => props.selected ? props.textColors[0] : props.textColors[1]};
+  transition: color 2s ease;
   cursor: default;
 `;
+
+const textPalette = {
+  about: ['rgb(94, 123, 168)', 'rgb(134, 91, 66)'],
+  coding: ['rgb(105, 106, 179)', 'rgb(112, 72, 48)'],
+  animation: ['rgb(247, 246, 176)', 'rgb(240, 212, 185)'],
+  contact: ['rgb(247, 176, 179)', 'rgb(130, 125, 177)']
+}
 
 const Sidebar = ({ focused, changeFocus }) => {
   const handleClick = (e) => {
@@ -26,10 +36,11 @@ const Sidebar = ({ focused, changeFocus }) => {
     <Wrapper onClick={handleClick}>
     {links.map((link) =>
       <Link
-        focused={link === focused}
+        textColors={textPalette[focused]}
+        selected={link === focused}
         id={link}
         key={link}>
-      {link}</Link>)}
+      {link[0].toUpperCase() + link.slice(1)}</Link>)}
     </Wrapper>
   )
 }
